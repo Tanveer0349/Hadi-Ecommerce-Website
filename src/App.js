9934c1f8aa056820410f63417113f435cfeb4667
Tanveer <sahiltanveerhussain@gmail.com>
2023-03-08 06:50:53 -0800

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,84 @@
+import React, { useState } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./common/header/Header";
+import Pages from "./pages/Pages";
+import Data from "./components/Data";
+import Cart from "./common/Cart/Cart";
+import Footer from "./common/footer/Footer";
+import Sdata from "./components/shops/Sdata";
+
+function App() {
+  const { productItems } = Data;
+  const { shopItems } = Sdata;
+
+  const [CartItem, setCartItem] = useState([]);
+
+  const addToCart = (product) => {
+    const productExist = CartItem.find((item) => item.id === product.id);
+    if (productExist) {
+      setCartItem(
+        CartItem.map((item) =>
+          item.id === product.id
+            ? { ...productExist, qty: productExist.qty + 1 }
+            : item
+        )
+      );
+    } else {
+      setCartItem([...CartItem, { ...product, qty: 1 }]);
+    }
+  };
+
+  const decreaseQty = (product) => {
+    const productExist = CartItem.find((item) => item.id === product.id);
+
+    if (productExist.qty === 1) {
+      setCartItem(CartItem.filter((item) => item.id !== product.id));
+    } else {
+      setCartItem(
+        CartItem.map((item) =>
+          item.id === product.id
+            ? { ...productExist, qty: productExist.qty - 1 }
+            : item
+        )
+      );
+    }
+  };
+
+  const removeItem = (product) => {
+    setCartItem(CartItem.filter((item) => item.id !== product.id));
+  };
+
+  return (
+    <>
+      <Router>
+        <Header CartItem={CartItem} />
+        <Switch>
+          <Route
+            path="/"
+            exact
+            render={(props) => (
+              <Pages
+                productItems={productItems}
+                addToCart={addToCart}
+                shopItems={shopItems}
+                {...props}
+              />
+            )}
+          />
+          <Route path="/cart" exact>
+            <Cart
+              CartItem={CartItem}
+              addToCart={addToCart}
+              decreaseQty={decreaseQty}
+              removeItem={removeItem}
+            />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
+    </>
+  );
+}
+
+export default App;
